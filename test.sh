46d9b3f6f22e3ab707c2df1a980a286b8641efec
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2023-11-07 15:40:36 -0700

    LOG GROUPSSSSS

--- test.sh
+++ test.sh
@@ -4,8 +4,7 @@ echo "--- HYDRATION"
 echo "Hydrating Llamas"
 echo "--- TESTING"
 echo "Testing Llamas"
-echo "--- RESULTS"
+echo "+++ RESULTS"
 echo "The results of the tests on the Llamas"
-echo "+++"
 
 exit 0
