4929c47c23706e6dc1806ea665b50480ad57604b
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2023-11-07 14:09:09 -0700

    One more modification to log group behaviour

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "+++"
-echo "+++ HYDRATION"
+echo "--- HYDRATION"
 echo "Hydrating Llamas"
 echo "--- TESTING"
 echo "Testing Llamas"
-echo "+++ RESULTS"
+echo "-- RESULTS"
 echo "The results of the tests on the Llamas"
 
 exit 0
