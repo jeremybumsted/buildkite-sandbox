75b173ff91ba0c22118f7e146cce52d710230b4c
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2023-11-07 14:13:21 -0700

    all groups collapsed

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "--- HYDRATION"
 echo "Hydrating Llamas"
 echo "--- TESTING"
 echo "Testing Llamas"
-echo "-- RESULTS"
+echo "--- RESULTS"
 echo "The results of the tests on the Llamas"
 
 exit 0
