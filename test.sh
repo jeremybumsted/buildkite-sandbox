edde5988952dbf22a99de32177c0c7d9094d65e9
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2023-11-07 14:06:27 -0700

    Add additional log groups

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "+++"
+echo "~~~ HYDRATION"
+echo "Hydrating Llamas"
+echo "~~~ TESTING"
+echo "Testing Llamas"
 echo "+++ RESULTS"
-echo "Testing the cool app"
+echo "The results of the tests on the Llamas"
 
 exit 0
