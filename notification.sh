6faab5e2873174f2696ccd0c62ec5b995ee0e361
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2023-12-05 12:09:54 -0700

    notifications appended to an existing job

---
+++ notification.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SLACK_USER=$1
+
+SLACK_MESSAGE="Hello yes it is I, a notification from Buildkite!"
+
+
+buildkite-agent step update "notify" '[
+  {
+    "slack": {
+      "channels": [
+        "jeremy-test#test-notifications"
+      ],
+      "message": "@'${SLACK_USER}': '${SLACK_MESSAGE}'"
+    }
+  }
+]' --debug-http --debug --step $BUILDKITE_STEP_ID --append
