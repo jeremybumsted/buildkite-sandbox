5ae2b3cf255f5265a888dcc24c0a8ee86813c62a
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2023-12-05 12:55:27 -0700

    single quote

--- notification.sh
+++ notification.sh
@@ -7,13 +7,13 @@ SLACK_USER=$1
 SLACK_MESSAGE="Hello yes it is I, a notification from Buildkite!"
 
 
-buildkite-agent step update "notify" "[
+buildkite-agent step update "notify" '[
     {
       "slack": {
         "channels": [
           "jeremy-test#test-notifications"
         ],
-        "message": "@${SLACK_USER}: ${SLACK_MESSAGE}"
-      }
+        "message": "@$SLACK_USER: $SLACK_MESSAGE"
+	}
     }
-]" --debug-http --debug --step "${BUILDKITE_STEP_ID}" --append
+]'--debug-http --debug --step "${BUILDKITE_STEP_ID}" --append
