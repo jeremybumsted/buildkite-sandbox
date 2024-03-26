e7d539e3678c4845649f4d8676f310ba8192dbac
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2024-03-26 13:16:38 -0600

    some debugging

--- terraform.sh
+++ terraform.sh
@@ -5,8 +5,15 @@ terraform plan -out=plan.log
 echo "<h4>terraform/$project Terraform plan</h4>" > plan.html
 echo '<details><summary>Terraform plan</summary>' >> plan.html
 echo '<div><code><pre class="term">' >> plan.html
+
+echo "~~~ doing the terminal thing"
 cat plan.log | terminal-to-html >> plan.html
 echo '</pre></code></div></details>' >> plan.html
+
+echo "~~~ here is the html"
+cat plan.html
+
+echo "~~~ Now we annotate"
 cat plan.html | head -c 1048576 | buildkite-agent annotate --style 'info' --context "ctx-plan-$project"z
 
 rm plan*
