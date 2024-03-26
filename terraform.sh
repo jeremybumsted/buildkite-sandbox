9c1e7dd2ec3588ce50fec43db4b454674edf874d
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2024-03-26 13:17:57 -0600

    fix annotation

--- terraform.sh
+++ terraform.sh
@@ -2,7 +2,7 @@
 
 terraform plan -out=plan.log
 
-echo "<h4>terraform/$project Terraform plan</h4>" > plan.html
+echo "<h4>Terraform plan</h4>" > plan.html
 echo '<details><summary>Terraform plan</summary>' >> plan.html
 echo '<div><code><pre class="term">' >> plan.html
 
@@ -14,6 +14,6 @@ echo "~~~ here is the html"
 cat plan.html
 
 echo "~~~ Now we annotate"
-cat plan.html | head -c 1048576 | buildkite-agent annotate --style 'info' --context "ctx-plan-$project"z
+cat plan.html | head -c 1048576 | buildkite-agent annotate --style 'info' --context "ctx-plan-result"
 
 rm plan*
