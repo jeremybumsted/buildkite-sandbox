590c1baf7122348863a0c9684eec7f8f4da606aa
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2024-03-27 14:03:49 -0600

    ?????

--- terraform.sh
+++ terraform.sh
@@ -4,12 +4,13 @@ terraform plan > plan.log
 cat plan.log
 
 project=${BUILDKITE_PIPELINE_SLUG}
+code_fence="\`\`\`"
 
 planAnnotationFile="plan.html"
-cat > "$planAnnotationFile" <<- EOF
-    \`\`\`term
-    This is a \033[0;31mtest\033[0m\n
-    \`\`\`
+cat > "$planAnnotationFile" <<EOF
+    ${code_fence}term
+    This is a \033[0;31mtest\033[0m
+    ${code_fence} \`\`\`
 EOF
 
 printf '%b\n' "$(cat $planAnnotationFile)" | buildkite-agent annotate \
