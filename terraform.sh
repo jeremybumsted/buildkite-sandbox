72dba28c1e22ba7d75172dd495e09c730da1e60c
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2024-03-27 13:17:31 -0600

    hmmmmmmmmmmmmmmmm

--- terraform.sh
+++ terraform.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 terraform plan > plan.log
+cat plan.log
 
 project=${BUILDKITE_PIPELINE_SLUG}
 
-code_fence='```'
-    planAnnotationFile="plan.html"
+code_fence="\`\`\`"
+planAnnotationFile="plan.html"
     cat > "$planAnnotationFile" <<- EOM
 <h4>terraform/$project Terraform plan</h4>
 <details><summary>Terraform plan</summary>
