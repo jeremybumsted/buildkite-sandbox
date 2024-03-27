d3841b7a6ac1474f8caeaab4dce48d4ddd31f261
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2024-03-27 13:19:28 -0600

    hmmmm more hardcoding?

--- terraform.sh
+++ terraform.sh
@@ -5,14 +5,13 @@ cat plan.log
 
 project=${BUILDKITE_PIPELINE_SLUG}
 
-code_fence="\`\`\`"
 planAnnotationFile="plan.html"
     cat > "$planAnnotationFile" <<- EOM
 <h4>terraform/$project Terraform plan</h4>
 <details><summary>Terraform plan</summary>
-${code_fence}term
+\`\`\`term
 $(cat plan.log)
-${code_fence}
+\`\`\`
 </details>
 EOM
 
