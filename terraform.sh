583c4802adb53aad9d466db1df2c500e500ae4cd
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2024-03-27 14:35:41 -0600

    maybe this

--- terraform.sh
+++ terraform.sh
@@ -6,16 +6,15 @@ cat plan.log
 project=${BUILDKITE_PIPELINE_SLUG}
 code_fence="\`\`\`"
 
-planAnnotationFile="plan.md"
+planAnnotationFile="plan.html"
 cat > "$planAnnotationFile" <<EOF
+    <h3>Terraform Plan</h3>
+    Here is the plan:
     ${code_fence}term
-    This is a \033[0;31mtest\033[0m
+        This is a \033[0;31mtest\033[0m
     ${code_fence}
 EOF
-printf '%b\n' "$(cat $planAnnotationFile)" | buildkite-agent annotate \
-    --style "info" \
-    --context "ctx-plan-$project"
 
-    # buildkite-agent annotate \
-    #   --style "info" \
-    #   --context "ctx-plan-$project" < "$planAnnotationFile"
+    buildkite-agent annotate \
+      --style "info" \
+      --context "ctx-plan-$project" < "$planAnnotationFile"
