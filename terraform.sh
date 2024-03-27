be6bd69bd7f49def5cdcdf8a8cfd1774400a2cd0
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2024-03-27 13:27:29 -0600

    no details

--- terraform.sh
+++ terraform.sh
@@ -9,14 +9,19 @@ code_fence="\`\`\`"
 
 planAnnotationFile="plan.html"
     cat > "$planAnnotationFile" <<- EOM
-<details>
-<summary>Hello</summary>
 ${code_fence}term
     World
 ${code_fence}
-</details>
 EOM
 
     buildkite-agent annotate \
       --style "info" \
       --context "ctx-plan-$project" < "$planAnnotationFile"
+
+
+# <h4>terraform/$project Terraform plan</h4>
+# <details>
+#     <summary>Terraform plan</summary>
+# ${code_fence}term
+#     $(cat plan.log)
+# ${code_fence}
